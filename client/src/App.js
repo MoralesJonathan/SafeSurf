8c6e5477c91a5d0ece193155528e1c38a45e4317
Fulton Garcia <fultongarcia@gmail.com>
2019-03-02 02:01:58 -0500

    cleaned up

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 import "./App.css";
 import AdForm from "./components/AdForm.js"
-import EmailListForm from "./components/EmailListForm.js"
-import AirlineSettings from "./components/AirlineSettings.js"
 import { Row, Col, Navbar } from "react-bootstrap";
 import SideNav from "./components/SideNav"
 import { BrowserRouter, Route, Switch } from 'react-router-dom'
-import Tracking from "./components/Tracking";
 
 class App extends Component {
   render() {
@@ -28,10 +25,7 @@ class App extends Component {
             </Col>
             <Col xs={9}>
               <Switch>
-                <Route path="/campaigns" component={AdForm} />
-                <Route path="/emailList" component={EmailListForm} />
-                <Route path="/tracking" component={Tracking} />
-                <Route exact path="/" component={AirlineSettings} />
+                <Route exact path="/" component={AdForm} />
               </Switch>
             </Col>
           </Row>
